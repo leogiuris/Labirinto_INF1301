848028bc85050e3a4527df3efe9eab16f48e0706
leogiuris <47838585+leogiuris@users.noreply.github.com>
2019-10-31 17:39:50 -0300

    matriz agora tá mais "modular"

--- MATRIZ.C
+++ MATRIZ.C
@@ -1,35 +1,34 @@
 /***************************************************************************
-*  $MCI Módulo de implementação: Módulo árvore
+*  $MCI Módulo de implementação: Módulo Matriz
 *
-*  Arquivo gerado:              ARVORE.C
-*  Letras identificadoras:      ARV
+*  Arquivo gerado:              Matriz.C
+*  Letras identificadoras:      MAT
 *
 *  Nome da base de software:    Exemplo de teste automatizado
 *  Arquivo da base de software: D:\AUTOTEST\PROJETOS\SIMPLES.BSW
 *
 *  Projeto: Disciplinas INF 1628 / 1301
 *  Gestor:  DI/PUC-Rio
-*  Autores: avs - Arndt von Staa
+*  Autores: Leonardo Santiago, Rafael Serpa
 *
 *  $HA Histórico de evolução:
 *     Versão  Autor    Data     Observações
-*       3.00   avs   28/02/2003 Uniformização da interface das funções e
+*       3.00   lgs   11/09/2019 Uniformização da interface das funções e
 *                               de todas as condições de retorno.
-*       2.00   avs   03/08/2002 Eliminação de código duplicado, reestruturação
-*       1.00   avs   15/08/2001 Início do desenvolvimento
+*       2.00   rs   08/09/2019 Adaptacao de codigo para multiplas matrizes
+*       1.00   dp   04/09/2019 Início do desenvolvimento
 *
 ***************************************************************************/
 
 #include	<malloc.h>
 #include	<stdio.h>
-
 #define MATRIZ_OWN
 #include "MATRIZ.H"
 #undef MATRIZ_OWN
 
 /***********************************************************************
 *
-*  $TC Tipo de dados: ARV Descritor do nó da árvore
+*  $TC Tipo de dados: MAT Descritor do nó da Matriz
 *
 *
 *  $ED Descrição do tipo
@@ -55,26 +54,27 @@ typedef struct tgNoMatriz {
 
 /***********************************************************************
 *
-*  $TC Tipo de dados: ARV Descritor da cabeça de uma árvore
+*  $TC Tipo de dados: MAT Descritor da cabeça de uma Matriz
 *
 *
 *  $ED Descrição do tipo
-*     A cabe‡a da árvore é o ponto de acesso para uma determinada árvore.
+*     A cabe‡a da Matriz é o ponto de acesso para uma determinada Matriz.
 *     Por intermédio da referência para o nó corrente e do ponteiro
-*     pai pode-se navegar a árvore sem necessitar de uma pilha.
-*     Pode-se, inclusive, operar com a árvore em forma de co-rotina.
+*     pai pode-se navegar a Matriz sem necessitar de uma pilha.
+*     Pode-se, inclusive, operar com a Matriz em forma de co-rotina.
 *
 ***********************************************************************/
 
 typedef struct tgMatriz {
 
 	tpNoMatriz * pNoOrigem ;
-               /* Ponteiro para a raiz da árvore */
+               /* Ponteiro para a raiz da Matriz */
 
     tpNoMatriz * pNoCorr ;
-               /* Ponteiro para o nó corrente da árvore */
+               /* Ponteiro para o nó corrente da Matriz */
 
-	int tamanho;
+	int linhas;
+	int colunas;
 
 } tpMatriz ;
 
@@ -92,44 +92,45 @@ void ExcluiNoLista(void * elem);
 
 void ResetaMat(tpMatriz * pMat);
 
+void MAT_RetornaOrigem(tpMatriz * pMat);
+
 /*****  Código das funções exportadas pelo módulo  *****/
 
 /***************************************************************************
 *
-*  Função: ARV Criar árvore
+*  Função: MAT Criar Matriz
 *  ****/
 
-MAT_tpCondRet MAT_CriarMatriz(tpMatriz **pMat, int tam)
+MAT_tpCondRet MAT_CriarMatriz(tpMatriz ** pMat, int lin, int col, void * valorInicial)
 {
 	int i,j;
 	tpNoMatriz ** mat;
-	printf("\n[CriarMatriz]\n");  
 
-	if(tam<1)
+	if(lin < 1 || col < 1)
 		return MAT_CondRetTamanhoInvalido;
-
+	
     if ( *pMat != NULL )
     {
-		//printf("\nah nao\n");
+		
         MAT_DestruirMatriz( *pMat ) ;
     } /* if */
-	//printf("aham\n");
+	
 	*pMat = ( tpMatriz * ) malloc( sizeof( tpMatriz )) ;
     if ( pMat == NULL )
     {
 		
 		return MAT_CondRetFaltouMemoria ;
     } /* if */
-	//printf("sei...\n");
-	mat = (tpNoMatriz**)malloc(tam*sizeof(tpNoMatriz*));
-	for(i=0; i<tam ;i++)
+	
+	mat = (tpNoMatriz**)malloc(lin*sizeof(tpNoMatriz*));
+	for(i=0; i<lin ;i++)
 	{
-		//printf("i\n");
-		mat[i] = (tpNoMatriz*)malloc(tam*sizeof(tpNoMatriz));
-		for(j=0; j<tam; j++)
+		
+		mat[i] = (tpNoMatriz*)malloc(col*sizeof(tpNoMatriz));
+		for(j=0; j<col; j++)
 		{
-			//printf("j\n");
-			mat[i][j].Valor = (void*)('a'+i*tam + j);
+			
+			mat[i][j].Valor = valorInicial;
 			mat[i][j].pNorte = NULL;
 			mat[i][j].pNordeste = NULL;
 			mat[i][j].pNoroeste = NULL;
@@ -140,32 +141,32 @@ MAT_tpCondRet MAT_CriarMatriz(tpMatriz **pMat, int tam)
 			mat[i][j].pSudoeste = NULL;
 		}
 	}
-
-	for(i=0; i<tam ;i++){
-		for(j=0; j<tam; j++){
-			mat[i][j].Valor = (void*)('a' +(i*tam) + j);
+	
+	for(i=0; i<lin ;i++){
+		for(j=0; j<col; j++){
+			
 			if(i>0)
 			{
 				
 				mat[i][j].pNorte = &mat[i-1][j];
 				if(j>0)
 					mat[i][j].pNoroeste = &mat[i-1][j-1];
-				if(j<tam)
+				if(j<col)
 					mat[i][j].pNordeste = &mat[i-1][j+1];
 			}
-			if(i<tam-1)
+			if(i<lin-1)
 			{
 				mat[i][j].pSul = &mat[i+1][j];
 				if(j>0)
 					mat[i][j].pSudoeste = &mat[i+1][j-1];
-				if(j<tam)
+				if(j<col)
 					mat[i][j].pSudeste = &mat[i+1][j+1];
 			}
 			if(j>0)
 			{
 				mat[i][j].pOeste = &mat[i][j-1];
 			}
-			if(j<tam-1)
+			if(j<col-1)
 			{
 				mat[i][j].pLeste = &mat[i][j+1];
 			}
@@ -175,61 +176,66 @@ MAT_tpCondRet MAT_CriarMatriz(tpMatriz **pMat, int tam)
 	
     (*pMat)->pNoOrigem = &mat[0][0] ;
 	(*pMat)->pNoCorr = &mat[0][0];
-	(*pMat)->tamanho = tam;
-	
-	//printf("krl mano...\n");
-	ImprimeMat(*pMat);
+	(*pMat)->linhas = lin;
+	(*pMat)->colunas = col;
+
+
     return MAT_CondRetOK ;
 
-}/* Fim função: ARV Destruir árvore */
+}/* Fim função: MAT Destruir Matriz */
 
 
 /***************************************************************************
 *
-*  Função: ARV Destruir árvore
+*  Função: MAT Destruir Matriz
 *  ****/
 
 void MAT_DestruirMatriz( tpMatriz * pMat )
 {	
 	
-	int fil = pMat->tamanho;
-	int i = fil;
-	pMat->pNoCorr = pMat->pNoOrigem;
-	printf("\n[DestroiMatriz]\n"); 
-	while(fil>1)
+	if(pMat!=NULL)
 	{
 		
-		for(i=fil;i>0;i--){
-			//printf("i");
-			pMat->pNoCorr = pMat->pNoCorr->pSul;
-		}
-		//printf("\naaaaaa\n");
-		DestroiMatriz(pMat->pNoCorr);
-		//printf("opa\n");
-		pMat->pNoCorr = pMat->pNoOrigem;
-		fil--;
-	}
-	//printf("foi?..\n");
-	DestroiMatriz(pMat->pNoCorr);
-	//printf("ae porra\n");
+		int i,lin = pMat->linhas;
 
-	pMat=NULL;
-	free(pMat);
-} /* Fim função: ARV Destruir árvore */
+		MAT_RetornaOrigem(pMat);
+
+		while(lin>1)
+		{	
+			
+			for(i=lin; i>1; i--)
+			{
+				
+				  MoveSul(pMat);
+			}
+			
+			DestroiMatriz(pMat->pNoCorr);
+			
+			MAT_RetornaOrigem(pMat);
+			lin--;
+		}
+	
+		DestroiMatriz(pMat->pNoCorr);
+		
+		
+		pMat=NULL;
+		free(pMat);
+		
+	}
+	
+} /* Fim função: MAT Destruir Matriz */
 
 
 /***************************************************************************
 *
-*  Função: ARV Destruir árvore
+*  Função: MAT Destruir Matriz
 *  ****/
 
 MAT_tpCondRet MAT_InsereValor(tpMatriz*pMatriz, void * Valor)
-{
-  
-  //tratar matriz nao existente
-
-	if(pMatriz->pNoOrigem==NULL)
+{ 
+	if(pMatriz==NULL)
 	{
+
 		return MAT_CondRetMatrizNaoExiste;
 	}
 	/*tratar falha no nó corrente*/
@@ -239,17 +245,43 @@ MAT_tpCondRet MAT_InsereValor(tpMatriz*pMatriz, void * Valor)
 	}
 	pMatriz->pNoCorr->Valor = Valor;
 	return MAT_CondRetOK;
-}/* Fim função: ARV Destruir árvore */
+}/* Fim função: MAT Destruir Matriz */
 
 /***************************************************************************
 *
-*  Função: ARV Destruir árvore
+*  Função: MAT Destruir Matriz
 *  ****/
 
-MAT_tpCondRet RetiraMatriz(tpMatriz*pMatriz)
+MAT_tpCondRet MAT_RetiraValor(tpMatriz**pMatriz)
 {
   //tratar matriz nao existente
-   if(pMatriz->pNoOrigem==NULL)
+   if(*pMatriz==NULL)
+	{
+
+		return MAT_CondRetMatrizNaoExiste;
+	}
+  /*tratar falha no nó corrente*/
+  if((*pMatriz)->pNoCorr==NULL)
+  {
+    return MAT_CondRetErroEstrutura;
+  }
+ (* pMatriz)->pNoCorr->Valor = NULL;
+  return MAT_CondRetOK;
+}
+
+/***************************************************************************
+*
+*  Função: MAT Move Matriz
+*  ****/
+
+MAT_tpCondRet MAT_ObterValor(tpMatriz*pMatriz, void ** ValorCorrente)
+{
+  if(pMatriz==NULL)
+	{
+
+		return MAT_CondRetMatrizNaoExiste;
+	}
+  if(pMatriz->pNoOrigem==NULL)
   {
     return MAT_CondRetMatrizNaoExiste;
   }
@@ -258,18 +290,21 @@ MAT_tpCondRet RetiraMatriz(tpMatriz*pMatriz)
   {
     return MAT_CondRetErroEstrutura;
   }
-  pMatriz->pNoCorr->Valor = NULL;
+
+  *ValorCorrente = pMatriz->pNoCorr->Valor;
   return MAT_CondRetOK;
 }
 
-/***************************************************************************
-*
-*  Função: ARV Move árvore
-*  ****/
+
 
 
 MAT_tpCondRet MoveLeste(tpMatriz*pMatriz)
 {
+
+	 if(pMatriz->pNoOrigem==NULL)
+  {
+    return MAT_CondRetMatrizNaoExiste;
+  }
   //tratar matriz nao existente
    if(pMatriz->pNoOrigem==NULL)
   {
@@ -429,108 +464,162 @@ MAT_tpCondRet MoveNoroeste(tpMatriz*pMatriz)
   return MAT_CondRetOK;
 }
 
-
-/***************************************************************************
-*
-*  Função: ARV Destruir árvore
-*  ****/
-
-MAT_tpCondRet ObterValor(tpMatriz*pMatriz, void *ValorCorrente)
+MAT_tpCondRet MoveCoord(tpMatriz *pMatriz, int x, int y)
 {
-  //tratar matriz nao existente
-  if(pMatriz->pNoOrigem==NULL)
-  {
-    return MAT_CondRetMatrizNaoExiste;
-  }
-  /*tratar falha no nó corrente*/
-  if(pMatriz->pNoCorr==NULL)
-  {
-    return MAT_CondRetErroEstrutura;
-  }
-  ValorCorrente = pMatriz->pNoCorr->Valor;
-  return MAT_CondRetOK;
+	MAT_tpCondRet ret;
+
+	if( x==0 && y==0 )
+		return;
+
+	if(x == 0 ^ y == 0){
+		if(x == 0)
+			if(y>0)
+				ret = MoveNorte(pMatriz);
+			else
+				ret = MoveSul(pMatriz);
+		else
+			if(x>0)
+				ret = MoveLeste(pMatriz);
+			else
+				ret = MoveOeste(pMatriz);
+	}
+	else{
+		if( x==y )
+			if(x>0)
+				ret = MoveNordeste(pMatriz);
+			else
+				ret = MoveSudoeste(pMatriz);
+		else
+			if(x>0)
+				ret = MoveNoroeste(pMatriz);
+			else
+				ret = MoveSudeste(pMatriz);
+	}
+
+	return ret;
 }
+
+
+
+
+
 
 /*****  Código das funções encapsuladas no módulo  *****/
 
-void ResetaCorr(tpMatriz * pMat)
+void MAT_RetornaOrigem(tpMatriz * pMat)
 {
 	pMat->pNoCorr = pMat->pNoOrigem;
-	return;
 }
+
 
 void ResetaMat(tpMatriz * pMat)
 {
 	int i,j,cont = 0;
-	int tam = pMat->tamanho;
-	for(i=0; i<tam; i++)
+	int lin = pMat->linhas;
+	int col = pMat->colunas;
+
+	for(i=0; i<lin; i++)
 	{
 		
-		pMat->pNoCorr = pMat->pNoOrigem;
+		MAT_RetornaOrigem(pMat);
 		while(cont!=i)
 		{
-			pMat->pNoCorr = pMat->pNoCorr->pSul;
+			  MoveSul(pMat);
 			cont++;
 		}cont = 0;
 
-		for(j=0; j<tam; j++)
+		for(j=0; j<col; j++)
 		{
-			pMat->pNoCorr->Valor = (void*)('a'+i*tam + j);
-			if(j<tam-1)
-				pMat->pNoCorr = pMat->pNoCorr->pLeste;
+			pMat->pNoCorr->Valor = (void*)(219);
+			if(j<col-1)
+				 MoveLeste(pMat);
 		}
 	}
 
 }
 
-void ImprimeMat(tpMatriz *pMat)
+
+char * ObtemMatrizString(tpMatriz * pMat)
 {
+	char * str;
 	int i,j,cont = 0;
-	int tam = pMat->tamanho;
+	int lin = pMat->linhas;
+	int col = pMat->colunas;
+	tpNoMatriz * temp = pMat->pNoCorr;
+	str = (char*)malloc(col*lin);
 	
-	//printf("\n[ImprimeMatriz]\n"); 
-	for(i=0; i<tam; i++)
+	
+	for(i=0; i<lin; i++)
 	{
-		printf("\n");
-		pMat->pNoCorr = pMat->pNoOrigem;
+		MAT_RetornaOrigem(pMat);
 		while(cont!=i)
 		{
-			pMat->pNoCorr = pMat->pNoCorr->pSul;
+			MoveSul(pMat);
 			cont++;
 		}cont = 0;
 
-		for(j=0; j<tam; j++)
+		for(j=0; j<col; j++)
 		{
-			
-			printf("%c	",(char)pMat->pNoCorr->Valor);
-			if(j<tam-1)
-				pMat->pNoCorr = pMat->pNoCorr->pLeste;
+			if(pMat->pNoCorr == temp)
+			{
+				str[i*col + j] = 'o';
+			}
+			else
+			{
+				str[i*col + j] = *(char*)pMat->pNoCorr->Valor;
+			}
+			if(j < col-1)
+				 MoveLeste(pMat);
 		}
+		
 	}
-	ResetaMat(pMat);
+	pMat->pNoCorr = temp;
+	return str;
+}
+
+
+void ImprimeMat(tpMatriz *pMat)
+{
+	int i,j,cont = 0;
+	int lin = pMat->linhas;
+	int col = pMat->colunas;
+	tpNoMatriz * temp = pMat->pNoCorr;
+	
+	for(i=0; i<lin; i++)
+	{
+		printf("\n");
+		MAT_RetornaOrigem(pMat);
+		while(cont!=i)
+		{
+			  MoveSul(pMat);
+			cont++;
+		}cont = 0;
+
+		for(j=0; j<col; j++)
+		{
+			if(pMat->pNoCorr == temp)
+				printf("o ");
+			else
+				printf("%c ",(char)pMat->pNoCorr->Valor);
+			if(j < col-1)
+				 MoveLeste(pMat);
+		}
+		
+	}
+	pMat->pNoCorr = temp;
 	printf("\n");
 }
 
 
 
 
-/***********************************************************************
-*
-*  $FC Função: ARV Destruir a estrutura da árvore
-*
-*  $EAE Assertivas de entradas esperadas
-*     pNoArvore != NULL
-*
-***********************************************************************/
+
 
 void DestroiMatriz(tpNoMatriz * pMat )
 {
-	//int i,cont = 0;
-	//printf("la vai...");
-	free(pMat);
 	
-	//printf("foi krl\n");
-	
+	free(pMat);	
 }
-/********** Fim do módulo de implementação: Módulo árvore **********/
+/********** Fim do módulo de implementação: Módulo Matriz **********/
 
+////
---
+++ labirinto.c
@@ -0,0 +1,405 @@
+#include"labirinto.h"
+#include "MATRIZ.H"
+#include"RESOLVEDOR.H"
+
+#define PAREDE	'@'
+#define CAMINHO ' '
+#define INICIO	'i'
+#define	FIM		'f'
+
+
+typedef struct tgLabirinto{
+	tpMatriz * mat;
+	int lin;
+	int col;
+}tpLabirinto;
+
+
+
+
+char * parede;
+char * caminho;
+char * inicio;
+char * fim;
+
+void ImprimeLabirinto(tpLabirinto * pLab);
+
+
+
+void Inicializa()
+{
+	parede = (char*)malloc(1);
+	caminho = (char*)malloc(1);
+	inicio = (char*)malloc(1);
+	fim = (char*)malloc(1);
+
+	*parede = PAREDE;
+	*caminho = CAMINHO;
+	*inicio = INICIO;
+	*fim = FIM;
+	
+	return;
+}
+
+
+LAB_tpCondRet LAB_CriarLabirinto(tpLabirinto ** pLab, int linhas, int colunas)
+{
+	tpMatriz * pMat = NULL;
+	MAT_tpCondRet ret;
+	
+	if(*pLab!=NULL) 
+		LAB_DestruirLabirinto(*pLab);
+
+	Inicializa();
+	*pLab = (tpLabirinto *) malloc(sizeof(tpLabirinto));
+
+	ret = MAT_CriarMatriz(&pMat,linhas,colunas,(void*)parede);
+
+	(*pLab)->mat = pMat;
+	(*pLab)->lin = linhas;
+	(*pLab)->col = colunas;
+	MAT_InsereValor((*pLab)->mat,(void*)inicio);
+	ImprimeLabirinto(*pLab);
+
+	return LAB_CondRetOK;
+}
+
+LAB_tpCondRet LAB_DestruirLabirinto(tpLabirinto * pLab)
+{
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+	
+	MAT_DestruirMatriz(pLab->mat);
+	free(pLab);
+	return LAB_CondRetOK;
+}
+
+int LAB_VerificarVizinho(tpLabirinto * pLab, int x, int y)
+{
+	void * temp;
+	char * valor = (char*)malloc(1);
+
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	MoveCoord(pLab->mat,x,y);
+	MAT_ObterValor(pLab->mat, &temp);
+	*valor = (char)temp;
+	MoveCoord(pLab->mat,-x,-y);
+
+	if(*valor == CAMINHO || *valor == FIM)
+		return 1;
+	else
+		return 0;
+
+}
+
+LAB_tpCondRet LAB_ObterValor(tpLabirinto * pLab, char * valor)
+{
+	//AE: Labirinto tem que existir
+	
+
+	void * temp;
+
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	MAT_ObterValor(pLab->mat, &temp);
+	
+	*valor = *(char*)temp;
+
+
+	return LAB_CondRetOK;
+}
+
+void LAB_RetornaInicio(tpLabirinto * pLab)
+{
+	MAT_RetornaOrigem(pLab->mat);
+}
+
+
+LAB_tpCondRet LAB_ConstroiLeste (tpLabirinto *pLab)
+{
+
+
+	//AE: Labirinto tem que existir
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	if(MoveLeste(pLab->mat) == MAT_CondRetNaoPossuiVizinho)
+	{
+		
+		return LAB_CondRetNaoPossuiVizinho;
+	}
+
+	MAT_InsereValor(pLab->mat,(void*)caminho);
+	
+	ImprimeLabirinto(pLab);
+	return LAB_CondRetOK;
+}
+
+LAB_tpCondRet LAB_ConstroiSul (tpLabirinto *pLab)
+{
+
+	//AE: Labirinto tem que existir
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	if(MoveSul(pLab->mat) == MAT_CondRetNaoPossuiVizinho)
+	{
+		
+		return LAB_CondRetNaoPossuiVizinho;
+	}
+
+	MAT_InsereValor(pLab->mat,(void*)caminho);
+	
+	ImprimeLabirinto(pLab);
+	return LAB_CondRetOK;
+}
+
+LAB_tpCondRet LAB_ConstroiNorte (tpLabirinto *pLab)
+{
+
+	//AE: Labirinto tem que existir
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	if(MoveNorte(pLab->mat) == MAT_CondRetNaoPossuiVizinho)
+	{
+		
+		return LAB_CondRetNaoPossuiVizinho;
+	}
+
+	MAT_InsereValor(pLab->mat,(void*)caminho);
+	
+	ImprimeLabirinto(pLab);
+	return LAB_CondRetOK;
+}
+
+LAB_tpCondRet LAB_ConstroiOeste (tpLabirinto *pLab)
+{
+
+	//AE: Labirinto tem que existir
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	if(MoveOeste(pLab->mat) == MAT_CondRetNaoPossuiVizinho)
+	{
+		
+		return LAB_CondRetNaoPossuiVizinho;
+	}
+	MAT_InsereValor(pLab->mat,(void*)caminho);
+	
+	ImprimeLabirinto(pLab);
+	return LAB_CondRetOK;
+}
+
+void ImprimeLabirinto(tpLabirinto * pLab)
+{
+	char * str;
+	int i,j,cont = 0;
+	int lin = pLab->lin;
+	int col = pLab->col;
+	system("cls");
+	str = ObtemMatrizString(pLab->mat);
+
+
+	printf(" %c",218);
+	for(i=0; i<2*col+1; i++)
+	{
+		printf("%c",196);
+	}
+	printf("%c",191);
+
+	for(i=0; i<lin; i++)
+	{
+		printf("\n");
+		printf(" | ");
+		for(j=0; j<col;j++)
+		{
+			printf("%c",str[i*col+j]);
+			if(j< col-1)
+				printf(" ");
+		}
+		printf(" |");
+	}
+
+	printf("\n %c", 192);
+	for(i=0; i<2*col+1; i++)
+	{
+		printf("%c",196);
+	}
+	printf("%c\n", 217);
+}
+
+
+LAB_tpCondRet LAB_FinalizaConstrucao(tpLabirinto * pLab)
+{
+	//AE: Labirinto tem que existir
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	MAT_InsereValor(pLab->mat,(void*)fim);
+	LAB_RetornaInicio(pLab);
+	ImprimeLabirinto(pLab);
+	return LAB_CondRetOK;
+}
+
+LAB_tpCondRet LAB_IrLeste(tpLabirinto * pLab)
+{
+
+	char * ret = (char*)malloc(1);
+
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	if(MoveLeste(pLab->mat) == MAT_CondRetNaoPossuiVizinho)
+	{
+		
+		return LAB_CondRetNaoPossuiVizinho;
+	}
+	LAB_ObterValor(pLab,ret);
+	if(*ret == PAREDE)
+	{
+		MoveOeste(pLab->mat);
+		return LAB_CondRetParede;
+	}
+
+	ImprimeLabirinto(pLab);
+	if(*ret== FIM)
+		return LAB_CondRetResolvido;
+
+	
+	return LAB_CondRetOK;
+}
+
+LAB_tpCondRet LAB_IrOeste(tpLabirinto * pLab)
+{
+	char * ret = (char*)malloc(1);
+
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	if(MoveOeste(pLab->mat) == MAT_CondRetNaoPossuiVizinho)
+	{
+		
+		return LAB_CondRetNaoPossuiVizinho;
+	}
+	LAB_ObterValor(pLab,ret);
+	if(*ret == PAREDE)
+	{
+		MoveLeste(pLab->mat);
+		return LAB_CondRetParede;
+	}
+
+	ImprimeLabirinto(pLab);
+	if(*ret== FIM)
+		return LAB_CondRetResolvido;
+
+	return LAB_CondRetOK;
+}
+
+LAB_tpCondRet LAB_IrSul(tpLabirinto * pLab)
+{
+	char * ret = (char*)malloc(1);
+
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	if(MoveSul(pLab->mat) == MAT_CondRetNaoPossuiVizinho)
+	{
+		
+		return LAB_CondRetNaoPossuiVizinho;
+	}
+	LAB_ObterValor(pLab,ret);
+	if(*ret == PAREDE)
+	{
+		MoveNorte(pLab->mat);
+		return LAB_CondRetParede;
+	}
+
+	ImprimeLabirinto(pLab);
+
+	if(*ret== FIM)
+		return LAB_CondRetResolvido;
+
+	
+	return LAB_CondRetOK;
+}
+
+LAB_tpCondRet LAB_IrNorte(tpLabirinto * pLab)
+{
+	char * ret = (char*)malloc(1);
+
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+	if(MoveNorte(pLab->mat) == MAT_CondRetNaoPossuiVizinho)
+	{
+		
+		return LAB_CondRetNaoPossuiVizinho;
+	}
+	LAB_ObterValor(pLab,ret);
+	if(*ret == PAREDE)
+	{
+		MoveSul(pLab->mat);
+		return LAB_CondRetParede;
+	}
+
+	ImprimeLabirinto(pLab);
+	if(*ret== FIM)
+		return LAB_CondRetResolvido;
+
+	
+	return LAB_CondRetOK;
+}
+
+LAB_tpCondRet LAB_IrCoord(tpLabirinto * pLab, int x, int y)
+{
+	char * ret = (char*)malloc(1);
+
+	if(pLab==NULL)
+		return LAB_CondRetLabirintoNaoExiste;
+
+
+
+	if(x == 0 && y == 0 ) return LAB_CondRetParede;
+
+
+
+	if(MoveCoord(pLab->mat,x,y) == MAT_CondRetNaoPossuiVizinho)
+	{
+		
+		return LAB_CondRetNaoPossuiVizinho;
+	}
+
+
+
+	LAB_ObterValor(pLab,ret);
+	if(*ret == PAREDE)
+	{
+		MoveCoord(pLab->mat,-x,-y);
+		return LAB_CondRetParede;
+	}
+
+	ImprimeLabirinto(pLab);
+
+	if(*ret== FIM)
+		return LAB_CondRetResolvido;
+
+	
+	return LAB_CondRetOK;
+}
+
+
+
+
+//void LAB_Resolver(tpLabirinto * pLab)
+//{
+//	RES_tpCondRet ret;
+//	ret = RES_ResolverLabirinto(pLab);
+//	if(ret == RES_CondRetResolvido)
+//		printf("foi\n");
+//	else
+//		printf("nao foi\n");
+//}
